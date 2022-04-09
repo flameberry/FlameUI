02f222b11ecfde7d1e4a68cc447a41488bbd4bd1
Flameberry <flameberrydev@gmail.com>
2022-04-09 20:11:45 +0530

    Did some refactoring.

--- Sandbox/src/FlameUILayer.cpp
+++ Sandbox/src/FlameUILayer.cpp
@@ -20,11 +20,6 @@ namespace Flameberry {
 
         FlameUI::Renderer::Init(rendererInitInfo);
 
-        // m_Panels.emplace_back(FlameUI::Panel::Create("Panel", glm::vec2{ 0.0f }, { 200.0f, 550.0f }, FL_PURPLE));
-        // m_Panels.emplace_back(FlameUI::Panel::Create("PanelOne", glm::vec2{ 0.0f }, { 300.0f, 600.0f }, FL_WHITE));
-        // m_Panels.emplace_back(FlameUI::Panel::Create("PanelTwo", glm::vec2{ 0.0f }, { 100.0f, 550.0f }, FL_BLUE));
-        // m_Panels.emplace_back(FlameUI::Panel::Create("PanelTwo", glm::vec2{ 0.0f }, { 300.0f, 300.0f }, FL_DARK_BLUE));
-
         FlameUI::EventPipeline::SubmitPanel("Panel", glm::vec2{ 0.0f }, { 200.0f, 550.0f }, FL_PURPLE);
         FlameUI::EventPipeline::SubmitPanel("PanelOne", glm::vec2{ 0.0f }, { 300.0f, 600.0f }, FL_WHITE);
         FlameUI::EventPipeline::SubmitPanel("PanelTwo", glm::vec2{ 0.0f }, { 100.0f, 550.0f }, FL_BLUE);
